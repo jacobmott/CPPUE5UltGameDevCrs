36edab0c999f217432d2e18c69d56f95804e90b0
Jacob Mott <jacobmott@gmail.com>
2023-06-10 15:58:12 -0500

    Finished Sections of course
    Section 6: Moving Objects With Code
    
    
    Former-commit-id: e664fceb511ed075cfcf84da4acaae20f0c41439

--- Source/CPPUE5UltGameDevCrs/Private/Items/Item.cpp
+++ Source/CPPUE5UltGameDevCrs/Private/Items/Item.cpp
@@ -11,6 +11,9 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+  ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+  RootComponent = ItemMesh;
+
 }
 
 // Called when the game starts or when spawned
@@ -25,14 +28,17 @@ void AItem::BeginPlay()
   //	GEngine->AddOnScreenDebugMessage(1, 60.0f, FColor::Cyan, FString("Item OnScreen Message"));
   //}
 
-	UWorld* World = GetWorld();
-
-	FVector Location = GetActorLocation();
-	FVector Forward = GetActorForwardVector();
-
-	DRAW_SPHERE(Location);
-  //DRAW_LINE(Location, Location + Forward * 100.f);
-  DRAW_VECTOR(Location, Location + Forward * 100.f);
+  //UWorld* World = GetWorld();
+  //
+  //SetActorLocation(FVector(0.f, 0.f, 100.f));
+  //SetActorRotation(FRotator(0.f, 45.f, 0.f));
+  //
+  //FVector Location = GetActorLocation();
+  //FVector Forward = GetActorForwardVector();
+  //
+  //DRAW_SPHERE(Location);
+  ////DRAW_LINE(Location, Location + Forward * 100.f);
+  //DRAW_VECTOR(Location, Location + Forward * 100.f);
 
 	
 }
@@ -57,5 +63,42 @@ void AItem::Tick(float DeltaTime)
   //
   //}
 
+
+  // Movement rate in units of cm/s
+  //float MovementRate = 50.f;
+  //float RotationRate = 45.f;
+
+  // MovementRate * DeltaTime (cm/s) * (s/frame) = (cm/frame)
+
+  //AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
+  //AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+
+
+  RunningTime += DeltaTime;
+
+  //float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+  //
+  //AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ)); 
+
+
+
+  //DRAW_SPHERE_SingleFrame(GetActorLocation());
+  //DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+  //
+  //FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+  //DRAW_POINT_SingleFrame(AvgVector);
+
 }
+
+float AItem::TransformedSin()
+{
+  return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+  return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
+
 
